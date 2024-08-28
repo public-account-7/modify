ba917abc19e2b0873502f3c4cee85e7dde6ab3c0
Infernus Scripts <139264639+InfernusScripts@users.noreply.github.com>
2024-08-28 03:53:31 +0300

    Update Loader.lua

--- Core/Scripts/Pressure/Loader.lua
+++ Core/Scripts/Pressure/Loader.lua
@@ -1180,9 +1180,10 @@ page:AddToggle({Caption = "No invisible walls", Callback = function(bool)
 end, Default = false})
 
 local page = window:AddPage({Title = "Anti-Monsters"})
-page:AddToggle({Caption = "God Mode\n(you won't complete no hiding challenge with this one, use auto hide instead)", Callback = function(bool)
+page:AddToggle({Caption = "God Mode", Callback = function(bool)
 	vals.GodMode = bool
 end, Default = false})
+page:AddLabel({Caption = "WARNING: You won't complete no hiding challenge with god mode! Use auto hide instead!"})
 page:AddToggle({Caption = "Auto Hide", Callback = function(bool)
 	vals.AH = bool
 end, Default = false})
